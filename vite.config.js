c7a270f4b467ce59554f50c214b166270954d585
Matthias Berndt <webmaster@euro-hoster.de>
2023-03-17 12:44:22 +0100

    #1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import copy from 'rollup-plugin-copy';
+import path from 'path';
 
+
+/**
 
 export default defineConfig({
     plugins: [
@@ -20,12 +23,34 @@ export default defineConfig({
     ],
 });
 
-module.exports = {
-    // ...
-    resolve: {
-        alias: {
-            '@fonts': path.resolve(__dirname, 'public/fonts')
-        }
-    },
-    // ...
-}
+ */
+
+export default defineConfig(({ command }) => {
+    const isProduction = command === 'build';
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+            copy({
+                targets: [
+                    { src: 'resources/fonts', dest: 'public' },
+                ],
+            }),
+        ],
+        css: {
+            loaderOptions: {
+                scss: {
+                    additionalData: `
+                        $fontsPath: "${isProduction ? path.resolve('public/fonts') : '/fonts'}";
+                        @import 'fonts';
+                    `,
+                },
+            },
+        },
+    };
+});
